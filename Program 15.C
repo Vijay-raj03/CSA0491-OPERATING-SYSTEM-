4fea19ea182bed336d8eedc58cb7fd7e8407c6d2
Vijay-raj03 <112714943+Vijay-raj03@users.noreply.github.com>
2023-01-12 09:59:30 +0530

    Create Program 15.C

---
+++ Program 15.C
@@ -0,0 +1,11 @@
+printf("\n Directorytfiles    | \n");
+		printf("+------------------------+");
+		for(j=0;j<n;j++){
+			printf("n %s",dir[j].dirName);
+			printf("\n %s",dir[j].dirName);
+			for(i=0;i<dir[j].fno;i++){
+				printf("n tt%s",dir[j].fileName[i]);
+				printf("\n tt%s",dir[j].fileName[i]);
+
+			}
+			printf("\n+------------------------+");
